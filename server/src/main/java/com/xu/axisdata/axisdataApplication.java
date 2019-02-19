9da5c2c6a81fa0ff8394475646f4a4d4ba406ff0
Kyo <32727925+Nikoace@users.noreply.github.com>
2019-02-20 00:52:07 +0800

    Initial commit

---
+++ server/src/main/java/com/xu/axisdata/axisdataApplication.java
@@ -0,0 +1,12 @@
+package com.xu.axisdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class axisdataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(axisdataApplication.class, args);
+  }
+}
